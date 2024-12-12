443c06029641d7bad6a4389bc87b3e32d3824fa4
MrLahoty <126816363+MrLahoty@users.noreply.github.com>
2024-12-12 19:57:31 +0530

    loveee

--- server/build/app.js
+++ server/build/app.js
@@ -22,10 +22,7 @@ exports.app.use(express_1.default.json({ limit: "50mb" }));
 // cookie parser
 exports.app.use((0, cookie_parser_1.default)());
 // cors => cross origin resource sharing
-exports.app.use((0, cors_1.default)({
-    origin: ['http://localhost:3000'],
-    credentials: true,
-}));
+exports.app.use((0, cors_1.default)());
 // api requests limit
 const limiter = (0, express_rate_limit_1.rateLimit)({
     windowMs: 15 * 60 * 1000,
